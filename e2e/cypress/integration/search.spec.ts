e504f09cbf0156a1b7cf299487d74c5cd354d311
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2021-02-19 19:45:33 +0200

    updated e2e tests and hours

--- e2e/cypress/integration/search.spec.ts
+++ e2e/cypress/integration/search.spec.ts
@@ -9,19 +9,19 @@ describe("Book search", () => {
 
     it("shows book with matching title", () => {
         cy.visit("/")
-        cy.get("[data-cy=search] > input").type("Te")
+        cy.get("[data-cy=search]").type("Te")
         cy.get("[data-cy=title]").contains("Test Book").should("exist")
     })
 
     it("does not show book with not matching title or author", () => {
         cy.visit("/")
-        cy.get("[data-cy=search] > input").type("Nope")
+        cy.get("[data-cy=search]").type("Nope{enter}")
         cy.get("[data-cy=title]").should("not.exist")
     })
 
     it("shows book with matching author", () => {
         cy.visit("/")
-        cy.get("[data-cy=search] > input").type("Author")
+        cy.get("[data-cy=search]").type("Author{enter}")
         cy.get("[data-cy=title]").contains("Test Book").should("exist")
     })
 })
