df138f2c0e89a9fa60f38418761f815eaa7211f9
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2021-02-24 11:19:50 +0200

    added shared folder to host shared types between backend and frontend

---
+++ shared/index.ts
@@ -0,0 +1,13 @@
+import { AddBookRequest, AddBookResponse } from "./api/AddBook"
+import { Author } from "./api/Author"
+import { Book } from "./api/Book"
+import { GetBooksRequest, GetBooksResponse } from "./api/GetBooks"
+
+export {
+    Book,
+    Author,
+    AddBookRequest,
+    AddBookResponse,
+    GetBooksRequest,
+    GetBooksResponse
+}
